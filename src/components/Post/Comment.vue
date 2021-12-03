46808a5aa1f4ce2685f7ac51c9e7662f02685e88
rico-sakspari <gitfreder@gmail.com>
2021-12-03 11:59:32 +0700

    refine design

--- src/components/Post/Comment.vue
+++ src/components/Post/Comment.vue
@@ -1,25 +1,30 @@
 <template>
   <v-card-actions>
     <v-list-item class="grow">
-      <v-list-item-avatar color="grey darken-3">
-        <v-img
-            class="elevation-6"
-            alt=""
-            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-        ></v-img>
-      </v-list-item-avatar>
-
-      <v-list-item-content>
-        <v-list-item-title>{{ username }}}</v-list-item-title>
-      </v-list-item-content>
-      <v-card-text class="text-h5 font-weight-bold"> {{ comment }}</v-card-text>
+      <v-col>
+        <v-row>
+          <v-list-item-avatar color="grey darken-3">
+            <v-img
+                class="elevation-6"
+                alt=""
+                src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+            ></v-img>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title class="fw-bold">{{ username }}</v-list-item-title>
+          </v-list-item-content>
+        </v-row>
+        <v-row class="">
+          <v-card-text>{{ comment }}</v-card-text>
+        </v-row>
+      </v-col>
     </v-list-item>
   </v-card-actions>
 </template>
 
 <script>
 export default {
-  props: ['username','comment'],
+  props: ['username', 'comment'],
   name: "Comment"
 }
 </script>
